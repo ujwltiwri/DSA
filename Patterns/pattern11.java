f85416f2ba4cca2e45a44ecb4d0eae6f3c8222d9
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-06-02 22:59:21 +0530

    pt11

--- Patterns/pattern11.java
+++ Patterns/pattern11.java
@@ -19,5 +19,13 @@ public class pattern11 {
             System.out.println();
         }
         scn.close();
+        //Output
+        /*
+         * 1
+         * 2 3
+         * 4 5 6
+         * 7 8 9 10
+         * 11 12 13 14 15
+         */
     }
 }
