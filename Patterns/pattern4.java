2bb503616ceb737109402b1558fa5fc65eab001d
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-06-02 14:12:34 +0530

    pattern 4

---
+++ Patterns/pattern4.java
@@ -0,0 +1,37 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int nst = n, nsp = 0;
+        for (int row = 1; row <= n; row++) {
+            // space
+            for (int space = 1; space <= nsp; space++) {
+                System.out.print("\t");
+            }
+
+            // stars
+            for (int stars = 1; stars <= nst; stars++) {
+                System.out.print("*\t");
+            }
+
+            // move to next line
+            System.out.println();
+            nst--;
+            nsp++;
+        }
+        // Output
+        /*
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+         */
+
+        scn.close();
+    }
+}
