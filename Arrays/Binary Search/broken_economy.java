7bc69ca7e61d25768e7403a89e6c438203e23817
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-07-02 22:24:43 +0530

    broken economy

---
+++ Arrays/Binary Search/broken_economy.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class broken_economy {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int k = scn.nextInt();
+
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] < k) {
+                low = mid + 1;
+            } else if (arr[mid] > k) {
+                high = mid - 1;
+            } else {
+                System.out.println(arr[mid]);
+                return;
+            }
+        }
+
+        System.out.println("Low: " + arr[low]);
+        System.out.println("High: " + arr[high]);
+
+        scn.close();
+    }
+}
+
+/*
+ * 
+ * In a country of novice government, the economic system is changed where only
+ * coins are used that too of various denominations. Whenever a foreigner visits
+ * this country, they visit a money exchanger to get the currency of the same
+ * country. As the foreigner is unaware of the denomination of the country, the
+ * money exchange prefers to tell them the denomination which is the nearest
+ * maximum and nearest minimum to the denomination mentioned by the foreigner.
+ * In case they get the correct guess of the denomination, they are told the
+ * same denomination. The denominations are always quoted in ascending order.
+ * 
+ * Example 1: In a country, 8 given denominations are as follows
+ * [5, 10, 15, 22, 33, 40, 42, 55]
+ * 
+ * The foreigner asks for denomination 25.
+ * The money exchange tells them that denominations of 33 and 22 are available.
+ * 
+ * Example 2:
+ * In a country, 5 given denominations are as follows
+ * [7, 14, 18, 25, 30]
+ * 
+ * The foreigner asks for the denomination of 18.
+ * 
+ * The money exchange tells them a denomination of 18 is available.
+ * 
+ * You are required to print the values told by the money exchange to the
+ * foreigner.
+ * 
+ * 1. You are given a number n, representing the size of array a.
+ * 2. You are given n numbers, representing elements of the array a.
+ * 3. You are given another number d.
+ * 4. You are required to find the ceil and floor of d in array a.
+ */
