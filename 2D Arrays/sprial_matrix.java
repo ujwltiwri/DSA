db1214b0dfb82b7cb8a4ef71a84c9b047f0ad3d1
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-07-07 23:33:58 +0530

    sprial matrix

--- 2D Arrays/sprial_matrix.java
+++ 2D Arrays/sprial_matrix.java
@@ -61,3 +61,46 @@ public class sprial_matrix {
         scn.close();
     }
 }
+
+/*
+ * 1. You are given a number n, representing the number of rows.
+ * 2. You are given a number m, representing the number of columns.
+ * 3. You are given n*m numbers, representing elements of 2d array a.
+ * 4. You are required to traverse and print the contents of the 2d array in
+ * form of a spiral.
+ * Input->
+ * 3
+ * 5
+ * 11
+ * 12
+ * 13
+ * 14
+ * 15
+ * 21
+ * 22
+ * 23
+ * 24
+ * 25
+ * 31
+ * 32
+ * 33
+ * 34
+ * 35
+ * 
+ * Output->
+ * 11
+ * 21
+ * 31
+ * 32
+ * 33
+ * 34
+ * 35
+ * 25
+ * 15
+ * 14
+ * 13
+ * 12
+ * 22
+ * 23
+ * 24
+ */
