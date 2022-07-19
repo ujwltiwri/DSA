fbf111509ae857c1eb5cd9b106bcc864c23a2a19
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-07-19 21:45:00 +0530

    array reverse

---
+++ Recursion/array_reverse.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class array_reverse {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        displayArrReverse(arr, 0);
+    }
+
+    public static void displayArrReverse(int[] arr, int idx) {
+        if (idx == arr.length) {
+            return;
+        }
+
+        displayArrReverse(arr, idx + 1);
+        System.out.println(arr[idx]);
+    }
+
+}
+
+/*
+ * You are required to print the elements of array from end to beginning each
+ * in a separate line.
+ */
