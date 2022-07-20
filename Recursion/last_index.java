e1aa812e4736e1eb77a4077f139438251b4a337c
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-07-20 19:24:24 +0530

    last index

--- Recursion/last_index.java
+++ Recursion/last_index.java
@@ -1,37 +1,77 @@
+// import java.io.*;
+// import java.util.*;
+
+// public class last_index {
+
+// public static void main(String[] args) throws Exception {
+// Scanner scn = new Scanner(System.in);
+// int n = scn.nextInt();
+// int[] arr = new int[n];
+// for (int i = 0; i < arr.length; i++) {
+// arr[i] = scn.nextInt();
+// }
+// int x = scn.nextInt();
+// int ans = lastIndex(arr, 0, x);
+// System.out.println(ans);
+// scn.close();
+// }
+
+// public static int lastIndex(int[] arr, int idx, int x) {
+
+// if (idx == arr.length)
+// return -1;
+
+// int recans = lastIndex(arr, idx + 1, x);
+
+// if (recans != -1) {
+// return recans;
+// } else {
+// if (arr[idx] == x) {
+// return idx;
+// } else {
+// return -1;
+// }
+// }
+// }
+
+// }
+
+// same question can be done in preorder as well
+
 import java.io.*;
 import java.util.*;
 
 public class last_index {
 
     public static void main(String[] args) throws Exception {
+        // write your code here
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
+
         int x = scn.nextInt();
-        int ans = lastIndex(arr, 0, x);
-        System.out.println(ans);
-        scn.close();
+
+        int answer = lastIndex(arr, n - 1, x);
+        System.out.println(answer);
     }
 
     public static int lastIndex(int[] arr, int idx, int x) {
-
-        if (idx == arr.length)
+        if (idx == -1) {
             return -1;
-
-        int recans = lastIndex(arr, idx + 1, x);
-
-        if (recans != -1) {
-            return recans;
-        } else {
-            if (arr[idx] == x) {
-                return idx;
-            } else {
-                return -1;
-            }
         }
+
+        if (arr[idx] == x) {
+            return idx;
+        }
+
+        int ans = lastIndex(arr, idx - 1, x);
+
+        return ans;
+
     }
 
 }
