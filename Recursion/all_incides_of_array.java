2eb9873e83e977bfb025a26bc0ba6ce5b8b127e4
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-07-21 19:13:33 +0530

    all incides of the array

---
+++ Recursion/all_incides_of_array.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class all_incides_of_array {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int x = Integer.parseInt(br.readLine());
+        int[] iarr = allIndices(arr, x, 0, 0);
+
+        if (iarr.length == 0) {
+            System.out.println("NO OUTPUT");
+            return;
+        }
+
+        for (int i = 0; i < iarr.length; i++) {
+            System.out.println(iarr[i]);
+        }
+    }
+
+    public static int[] allIndices(int[] arr, int x, int idx, int count) {
+
+        if (idx == arr.length) {
+            int[] retarr = new int[count];
+            return retarr;
+        }
+
+        if (arr[idx] == x) {
+            count++;
+        }
+
+        int[] recarr = allIndices(arr, x, idx + 1, count);
+
+        if (arr[idx] == x) {
+            // filling of the array
+            recarr[count - 1] = idx;
+            return recarr;
+        } else {
+            return recarr;
+        }
+    }
+
+}
