9e9b512a40b9671c039087641ccb4a5ed81b620b
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-07-28 23:37:03 +0530

    Print Subsequence

--- Recursion/print_subsequence.java
+++ Recursion/print_subsequence.java
@@ -3,23 +3,45 @@ import java.util.*;
 
 public class print_subsequence {
     // Bottom to Top Approach is Used to Save Memory
+    // public static void main(String[] args) throws Exception {
+    // Scanner scn = new Scanner(System.in);
+    // String str = scn.nextLine();
+    // printSS(str, "");
+    // }
+
+    // public static void printSS(String str, String ans) {
+    // if (str.length() == 0) {
+    // System.out.println(ans);
+    // return;
+    // }
+
+    // char ch = str.charAt(0);
+    // String ros = str.substring(1);
+
+    // printSS(ros, ans + ch); // for yes
+    // printSS(ros, ans); // for no
+    // }
+
+    // if also asked to return count then the code will be like this
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
-        printSS(str, "");
+        System.out.println(printSS(str, ""));
     }
 
-    public static void printSS(String str, String ans) {
+    public static int printSS(String str, String ans) {
         if (str.length() == 0) {
             System.out.println(ans);
-            return;
+            return 1;
         }
-
+        int count = 0;
         char ch = str.charAt(0);
         String ros = str.substring(1);
 
-        printSS(ros, ans + ch); // for yes
-        printSS(ros, ans); // for no
+        count += printSS(ros, ans + ch); // for yes
+        count += printSS(ros, ans); // for no
+
+        return count;
     }
 
 }
