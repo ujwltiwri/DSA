0f7e50651bafee6a3839b53ecec60cd99b1fabd4
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-08-13 23:36:51 +0530

    PQ

--- HashMap and Heap/prority_queue_intro.java
+++ HashMap and Heap/prority_queue_intro.java
@@ -20,10 +20,9 @@ class prority_queue_intro {
     // It should print required output.
     public static void solve(int n, int[] arr, int k) {
         // Priority Queue Introduction
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // This is Normal Priority Queue -> Elements Are Added &
-                                                           // Removed in Normal Order i.e. Increasing
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        // //Here Elements Are Added & Removed in Reverse Order i.e. Decreasing
+        /* This is Normal Priority Queue -> Elements Are Added & Removed in Normal Order i.e. Increasing -> Low to High */ 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        /* This is Reversed Priority Queue -> Elements Are Added & Removed in Reversed Order i.e. Decreasing -> High to Low */
         // 1-> Add into Priority Queue
         pq.add(5);
         pq.add(10);
@@ -38,7 +37,6 @@ class prority_queue_intro {
         }
 
         // 3 -> You Can Access the Top Most Element in Priority Queue By Using Peek()
-
         System.out.println(pq.peek()); // 5 -> In Case of Normal Priority Queue
         System.out.println(pq.peek()); // 13 -> In Case of Reversed Priority Queue
 
